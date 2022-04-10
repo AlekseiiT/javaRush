a5c007cc15e50b5f5ec50e610dd6a6037622ae62
AlexeyTarasovv <coolerr195@gmail.com>
2022-04-10 15:14:48 +0300

    solve list tasks

---
+++ src/com/company/task_7_9_5/Solution.java
@@ -0,0 +1,24 @@
+package com.company.task_7_9_5;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/*
+LinkedList и ArrayList
+*/
+
+public class Solution {
+    public static Object createArrayList() {
+        ArrayList al = new ArrayList();
+        return al;
+    }
+
+    public static Object createLinkedList() {
+        LinkedList ll = new LinkedList();
+        return ll;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
