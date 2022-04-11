c39e5cd38afaee82c03927a95dc9f87e62a08670
alekseiTar <aleksei_tarasov@senla.eu>
2022-04-11 14:13:19 +0300

    solve task to delete duplicate values in map

---
+++ src/com/company/CollectionPractice/task194/Solution.java
@@ -0,0 +1,56 @@
+package com.company.CollectionPractice.task194;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+Нам повторы не нужны
+*/
+
+public class Solution {
+    public static Map<String, String> createMap() {
+        Map<String, String> map = new HashMap<>();
+        map.put("Смирнов", "Сильвестр");
+        map.put("Иванов", "Сильвестр");
+        map.put("Кузнецов", "Сильвестр");
+        map.put("Соколов", "Сильвестр");
+        map.put("Попов", "Максим");
+        map.put("Лебедев", "Ярослав");
+        map.put("Новиков", "Александр");
+        map.put("Козлов", "Михаил");
+        map.put("Морозов", "Илья");
+        map.put("Петров", "Алексей");
+        return map;
+    }
+
+    public static void removeTheFirstNameDuplicates(Map<String, String> map) {
+        Map<String, String> copy = new HashMap<>(map);
+
+        for (String name : copy.values()) {
+            int count = 0;
+            for (String nameTmp : map.values()) {
+                if (nameTmp.equals(name)) {
+                    count++;
+                }
+            }
+            if (count > 1)
+                removeItemFromMapByValue(map, name);
+        }
+    }
+
+    public static void removeItemFromMapByValue(Map<String, String> map, String value) {
+        Map<String, String> copy = new HashMap<>(map);
+        for (Map.Entry<String, String> pair : copy.entrySet()) {
+            if (pair.getValue().equals(value)) {
+                map.remove(pair.getKey());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Map<String, String> map = createMap();
+        removeTheFirstNameDuplicates(map);
+        System.out.println(map);
+    }
+}
+
