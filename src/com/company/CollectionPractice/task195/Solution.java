6d7de0e936054b7ec01351d7695a9c51506e9197
alekseiTar <aleksei_tarasov@senla.eu>
2022-04-11 15:49:55 +0300

    solve task to delete value under 500 from map, typo

--- src/com/company/CollectionPractice/task195/Solution.java
+++ src/com/company/CollectionPractice/task195/Solution.java
@@ -9,18 +9,18 @@ import java.util.Map;
 
 public class Solution {
     public static Map<String, Integer> createMap() {
-        Map<String, Integer> map1 = new HashMap<>();
-        map1.put("Смирнов", 500);
-        map1.put("Иванов", 300);
-        map1.put("Кузнецов", 2000);
-        map1.put("Соколов", 550);
-        map1.put("Попов", 200);
-        map1.put("Лебедев", 600);
-        map1.put("Новиков", 700);
-        map1.put("Козлов", 850);
-        map1.put("Морозов", 725);
-        map1.put("Петров", 530);
-        return map1;
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Смирнов", 500);
+        map.put("Иванов", 300);
+        map.put("Кузнецов", 2000);
+        map.put("Соколов", 550);
+        map.put("Попов", 200);
+        map.put("Лебедев", 600);
+        map.put("Новиков", 700);
+        map.put("Козлов", 850);
+        map.put("Морозов", 725);
+        map.put("Петров", 530);
+        return map;
     }
 
     public static void removeItemFromMap(Map<String, Integer> map) {
