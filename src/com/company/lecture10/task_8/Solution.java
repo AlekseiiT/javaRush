1b108e5cc1eb784217f3bcb7b95dcf668a2aaf3a
AlexeyTarasovv <coolerr195@gmail.com>
2022-04-20 11:03:16 +0300

    start OOP

---
+++ src/com/company/lecture10/task_8/Solution.java
@@ -0,0 +1,38 @@
+package com.company.lecture10.task_8;
+
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] data = new int[]{1, 2, 3, 5, -2, -8, 0, 77, 5, 5};
+
+        Pair result = getMinimumMaximumPair(data);
+
+        System.out.println("The minimum is " + result.x);
+        System.out.println("The maximum is " + result.y);
+    }
+
+    public static Pair getMinimumMaximumPair(int[] inputArray) {
+        if (inputArray == null || inputArray.length == 0) {
+            return new Pair(null, null);
+        }
+
+        int[] copy = inputArray.clone();
+
+        Arrays.sort(copy);
+
+        return new Pair(copy[0], copy[copy.length-1]);
+    }
+
+    public static class Pair {
+        public Integer x;
+        public Integer y;
+
+        public Pair(Integer x, Integer y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
+
---
+++ src/com/company/lecture12/task_3/Solution.java
@@ -0,0 +1,27 @@
+package com.company.lecture12.task_3;
+
+/*
+Класс Human и интерфейсы CanRun, CanSwim
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public abstract class Human implements CanRun, CanSwim
+    {
+
+    }
+
+    public interface CanRun
+    {
+        void run();
+    }
+    public interface CanSwim
+    {
+        void swim();
+    }
+    //add public interfaces and a public class here - добавь public интерфейсы и public класс тут
+}
+
