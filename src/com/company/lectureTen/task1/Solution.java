479f25ffd39f5fe999dcd7a75c95ff17b700e1c2
AlexeyTarasovv <coolerr195@gmail.com>
2022-04-23 23:28:20 +0300

    some task, cat factory

---
+++ src/com/company/lectureTen/task1/Solution.java
@@ -0,0 +1,18 @@
+package com.company.lectureTen.task1;
+
+import java.io.*;
+
+/*
+Обогатим код функциональностью!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 44;
+        int b = 300;
+        short c = (short) (b - a);
+        System.out.println(c);
+    }
+}
+
+
