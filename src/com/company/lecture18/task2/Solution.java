b33ab9584d5a02e7ca7cfb85e31a6e2e795cf082
AlexeyTarasovv <coolerr195@gmail.com>
2022-05-23 12:24:02 +0300

    level 18 tasks 1

--- src/com/company/lecture18/task2/Solution.java
+++ src/com/company/lecture18/task2/Solution.java
@@ -1,52 +1,26 @@
 package com.company.lecture18.task2;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
 /*
-Синхронизированные методы
+Минимальный байт
 */
 
 public class Solution {
-    private double param = Math.random();
-    private StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception {
+        String path = new BufferedReader(new InputStreamReader(System.in)).readLine();
 
-    private synchronized void method0() {
-        Double d = method3();
-    }
+        FileInputStream fileInputStream = new FileInputStream(path);
 
-    protected void method1(String param1) {
-        Solution solution = new Solution();
-        solution.method0();
-    }
+        int minByte = 255;
 
-    public void method2(int param1) {
-        param1++;
-    }
-
-    synchronized double method3() {
-        double random = Math.random();
-        param += 40.7;
-        return random + param;
-    }
-
-    private void method4() {
-        sb.append(1).append(1).append(1).append(1);
-    }
-
-    protected void method5(String param2) {
-        new StringBuffer().append(param2).append(param2).append(param2);
-    }
-
-    public String method6(int param2) {
-        System.out.println("Thinking....");
-        method7(5e-2);
-        sb = new StringBuilder("Got it!.");
-        return sb.toString();
-    }
-
-    String method7(double param2) {
-        return "" + param2;
-    }
-
-    public static void main(String[] args) {
-
+        while (fileInputStream.available() > 0) {
+            int data = fileInputStream.read();
+            minByte = Math.min(data, minByte);
+        }
+        fileInputStream.close();
+        System.out.println(minByte);
     }
 }
---
+++ src/com/company/lecture18/task3/Solution.java
@@ -0,0 +1,35 @@
+package com.company.lecture18.task3;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/*
+Самые частые байты
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        String path = new BufferedReader(new InputStreamReader(System.in)).readLine();
+
+        int[] byteCountArray = new int[256];
+        try (FileInputStream fileInputStream = new FileInputStream(path)) {
+            while (fileInputStream.available() > 0){
+                byteCountArray[fileInputStream.read()] += 1;
+            }
+        }
+
+        int maxCount = 0;
+        for (int byteCount : byteCountArray) {
+            if (maxCount < byteCount) maxCount = byteCount;
+        }
+
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < byteCountArray.length; i++) {
+            if (byteCountArray[i] == maxCount) result.add(i);
+        }
+
+        for (int resultItem : result) System.out.print(resultItem + " ");
+    }
+}
