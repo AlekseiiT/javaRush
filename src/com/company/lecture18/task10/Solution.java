3a9a7eb8ed1dd243fed39e8172facdd6ada0b99c
AlexeyTarasovv <coolerr195@gmail.com>
2022-05-26 17:50:08 +0300

    level 18 tasks 5

---
+++ src/com/company/lecture18/task10/Solution.java
@@ -0,0 +1,55 @@
+package com.company.lecture18.task10;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/*
+Разделение файла
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        new Thread(new DecoratorRunnableImpl(new DecoratorMyRunnableImpl(new RunnableImpl()))).start();
+    }
+
+    public static class RunnableImpl implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("RunnableImpl body");
+        }
+    }
+
+    public static class DecoratorRunnableImpl implements Runnable {
+        private Runnable component;
+
+        public DecoratorRunnableImpl(Runnable component) {
+            this.component = component;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("DecoratorRunnableImpl body");
+            component.run();
+        }
+    }
+
+    public static class DecoratorMyRunnableImpl implements Runnable {
+        private Runnable component;
+
+        public DecoratorMyRunnableImpl(Runnable component) {
+            this.component = component;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("DecoratorMyRunnableImpl body");
+            component.run();
+        }
+    }
+}
+
---
+++ src/com/company/lecture18/task11/AmigoOutputStream.java
@@ -0,0 +1,17 @@
+package com.company.lecture18.task11;
+
+import java.io.IOException;
+
+public interface AmigoOutputStream {
+    void flush() throws IOException;
+
+    void write(int b) throws IOException;
+
+    void write(byte[] b) throws IOException;
+
+    void write(byte[] b, int off, int len) throws IOException;
+
+    void close() throws IOException;
+}
+
+
