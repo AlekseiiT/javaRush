09695bcdddb3427aedd51d101a2e1d735dcfe698
Aleksey Tarasov <coolerr195@gmail.com>
2022-10-10 12:43:45 +0300

    finish crud task with fileReader and fileWriter for updating and deleting

--- src/com/company/questCore/level8/lecture11/Product.java
+++ src/com/company/questCore/level8/lecture11/Product.java
@@ -3,18 +3,18 @@ package com.company.questCore.level8.lecture11;
 public class Product {
     int id;
     String name;
-    String productName;
+    String price;
     String quantity;
 
     public Product(int id, String name, String productName, String quantity){
         this.id = id;
         this.name = name;
-        this.productName = productName;
+        this.price = productName;
         this.quantity = quantity;
     }
 
     @Override
     public String toString() {
-        return String.format("%08d %s %s %s%n", id, name, productName, quantity);
+        return String.format("%08d %s %s %s%n", id, name, price, quantity);
     }
 }
--- src/com/company/questCore/level8/lecture11/Solution.java
+++ src/com/company/questCore/level8/lecture11/Solution.java
@@ -5,7 +5,6 @@ import com.company.questCore.level8.lecture11.constants.TaskConstants;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Solution {
 
@@ -29,18 +28,53 @@ public class Solution {
             }
         }
 
-        switch (args[0]){
-            case "-c":
+        switch (args[0]) {
+            case "-c": {
                 int id = 0;
-                for (Product product : products){
+                for (Product product : products) {
                     if (product.id > id) {
                         id = product.id;
                     }
                 }
                 Product product = new Product(++id, args[1], args[2], args[3]);
-                try(FileWriter fileWriter = new FileWriter(path, true)){
-                    fileWriter.write(product.toString());
+                products.add(product);
+                break;
+            }
+            case "-u": {
+                Product productToUpdate = null;
+                for (Product product : products) {
+                    if (product.id == Integer.parseInt(args[1])) {
+                        productToUpdate = product;
+                        product.name = args[2];
+                        product.price = args[3];
+                        product.quantity = args[4];
+                    }
                 }
+                if (productToUpdate == null) {
+                    System.out.println("There's no such product available!");
+                    return;
+                }
+                break;
+            }
+            case "-d": {
+                int id = Integer.parseInt(args[1]);
+                Product productToDelete = null;
+                for (Product product : products) {
+                    if (id == product.id) {
+                        productToDelete = product;
+                    }
+                }
+                if (productToDelete == null) {
+                    System.out.println("There's no such product to delete!");
+                    break;
+                }
+                products.remove(productToDelete);
+            }
+        }
+        try(FileWriter fileWriter = new FileWriter(path)) {
+            for (Product product : products){
+                fileWriter.write(product.toString());
+            }
         }
     }
 
