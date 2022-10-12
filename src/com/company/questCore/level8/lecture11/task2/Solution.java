4023265a3945050130238eee46c7bc95a14d888c
Aleksey Tarasov <coolerr195@gmail.com>
2022-10-12 17:28:33 +0300

    refactor crudTask code

--- src/com/company/questCore/level8/lecture11/task2/Solution.java
+++ src/com/company/questCore/level8/lecture11/task2/Solution.java
@@ -8,73 +8,41 @@ import java.util.List;
 
 public class Solution {
 
+    /*
+    * to add: -c productName price quantity
+    * to update: -u id productName price quantity
+    * to delete: -d id
+    * */
+
     public static void main(String[] args) throws IOException {
 
         System.out.println(TaskConstants.getDataTxtPath());
-        if(args.length == 0){
-            System.out.println("Submit parameters!");
-            return;
-        }
+        assertArgs(args);
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String path = reader.readLine();
-
-        List<Product> products = new ArrayList<>();
-
-        try(BufferedReader fileReader = new BufferedReader(new FileReader(path))) {
-            while (fileReader.ready()) {
-                Product product = getProduct(fileReader.readLine());
-                products.add(product);
-            }
-        }
+        String path = getPathToTheFile();
+        List<Product> products = getListOfProductsFromTheFile(path);
 
         switch (args[0]) {
             case "-c": {
-                int id = 0;
-                for (Product product : products) {
-                    if (product.id > id) {
-                        id = product.id;
-                    }
-                }
-                Product product = new Product(++id, args[1], args[2], args[3]);
-                products.add(product);
+                addNewProductToTheList(products, args);
                 break;
             }
             case "-u": {
-                Product productToUpdate = null;
-                for (Product product : products) {
-                    if (product.id == Integer.parseInt(args[1])) {
-                        productToUpdate = product;
-                        product.name = args[2];
-                        product.price = args[3];
-                        product.quantity = args[4];
-                    }
-                }
-                if (productToUpdate == null) {
-                    System.out.println("There's no such product available!");
-                    return;
-                }
+                updateProductInTheList(products, args);
                 break;
             }
             case "-d": {
-                int id = Integer.parseInt(args[1]);
-                Product productToDelete = null;
-                for (Product product : products) {
-                    if (id == product.id) {
-                        productToDelete = product;
-                    }
-                }
-                if (productToDelete == null) {
-                    System.out.println("There's no such product to delete!");
-                    break;
-                }
-                products.remove(productToDelete);
+                deleteProductFromTheList(products, args);
+                break;
             }
         }
-        try(FileWriter fileWriter = new FileWriter(path)) {
-            for (Product product : products){
-                fileWriter.write(product.toString());
-            }
+        writeProductsIntoTheFile(products, path);
+    }
+
+    public static void assertArgs(String[] args){
+        if(args.length == 0){
+            System.out.println("Submit parameters!");
+            System.exit(0);
         }
     }
 
@@ -85,5 +53,83 @@ public class Solution {
         String price = productInfo[2];
         String quantity = productInfo[3];
         return new Product(Integer.parseInt(id), productName, price, quantity);
+    }
+
+    public static String getPathToTheFile() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String path;
+        try {
+            path = reader.readLine();
+        } catch (IOException e){
+            throw new RuntimeException();
+        }
+        return path;
+    }
+
+    public static List<Product> getListOfProductsFromTheFile(String path){
+        List<Product> products = new ArrayList<>();
+
+        try(BufferedReader fileReader = new BufferedReader(new FileReader(path))) {
+            while (fileReader.ready()) {
+                Product product = getProduct(fileReader.readLine());
+                products.add(product);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    return products;
+    }
+
+    public static void addNewProductToTheList(List<Product> products, String[] args){
+        int id = 0;
+        for (Product product : products) {
+            if (product.id > id) {
+                id = product.id;
+            }
+        }
+        Product product = new Product(++id, args[1], args[2], args[3]);
+        products.add(product);
+    }
+
+    public static void updateProductInTheList(List<Product> products, String[] args){
+        boolean isUpdated = false;
+
+        for (Product product : products) {
+            if (product.id == Integer.parseInt(args[1])) {
+                product.name = args[2];
+                product.price = args[3];
+                product.quantity = args[4];
+                isUpdated = true;
+            }
+        }
+        if (!isUpdated) {
+            System.out.println("There's no such product available!");
+            System.exit(0);
+        }
+    }
+
+    public static void deleteProductFromTheList(List<Product> products, String[] args){
+        int id = Integer.parseInt(args[1]);
+        Product productToDelete = null;
+        for (Product product : products) {
+            if (id == product.id) {
+                productToDelete = product;
+            }
+        }
+        if (productToDelete == null) {
+            System.out.println("There's no such product to delete!");
+            System.exit(0);
+        }
+        products.remove(productToDelete);
+    }
+
+    public static void writeProductsIntoTheFile(List<Product> products, String path){
+        try(FileWriter fileWriter = new FileWriter(path)) {
+            for (Product product : products){
+                fileWriter.write(product.toString());
+            }
+        } catch (IOException e){
+            throw new RuntimeException();
+        }
     }
 }
