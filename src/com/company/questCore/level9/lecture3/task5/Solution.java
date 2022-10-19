adb1b41f22013daf1af9993a0be804fd965aca64
Aleksey Tarasov <coolerr195@gmail.com>
2022-10-19 21:22:58 +0300

    one more with adapter pattern

---
+++ src/com/company/questCore/level9/lecture3/task5/Solution.java
@@ -0,0 +1,84 @@
+package com.company.questCore.level9.lecture3.task5;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public static Map<String, String> countries = new HashMap<String, String>();
+
+    static {
+        countries.put("RU", "Russia");
+        countries.put("UA", "Ukraine");
+        countries.put("CA", "Canada");
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class DataAdapter implements RowItem {
+
+        private Customer customer;
+        private Contact contact;
+
+        public DataAdapter(Customer customer, Contact contact) {
+            this.customer = customer;
+            this.contact = contact;
+        }
+
+        @Override
+        public String getCountryCode() {
+            for (Map.Entry<String, String> entry : countries.entrySet()){
+                if (entry.getValue().equalsIgnoreCase(customer.getCountryName()))
+                    return entry.getKey();
+            }
+            return null;
+        }
+
+        @Override
+        public String getCompany() {
+            return customer.getCompanyName();
+        }
+
+        @Override
+        public String getContactFirstName() {
+            //For example: Ivanov, Ivan
+            return contact.getName().split(", ")[1];
+        }
+
+        @Override
+        public String getContactLastName() {
+            return contact.getName().split(", ")[0];
+        }
+
+        @Override
+        public String getDialString() {
+            String phone = contact.getPhoneNumber().replaceAll("\\D", "");
+            return String.format("callto://+%s", phone);
+        }
+    }
+
+    public static interface RowItem {
+        String getCountryCode();        //For example: UA
+
+        String getCompany();            //For example: JavaRush Ltd.
+
+        String getContactFirstName();   //For example: Ivan
+
+        String getContactLastName();    //For example: Ivanov
+
+        String getDialString();         //For example: callto://+380501234567
+    }
+
+    public static interface Customer {
+        String getCompanyName();        //For example: JavaRush Ltd.
+
+        String getCountryName();        //For example: Ukraine
+    }
+
+    public static interface Contact {
+        String getName();               //For example: Ivanov, Ivan
+
+        String getPhoneNumber();        //For example: +38(050)123-45-67 or +3(805)0123-4567 or +380(50)123-4567 or ...
+    }
+}
